474075f359a1e6ef68e7a48f06bbe9a13857dc88
lily <chishin004@gmail.com>
2021-07-15 15:48:25 +0800

    7/15  js-caculation finish

--- calculator/index3.js
+++ calculator/index3.js
@@ -1,24 +1,68 @@
-let tatal = document.getElementById("tatal");
-tatal = [];
-console.log(tatal);
+let total = document.getElementById("total");
+total = []; // rename
 
-function push(a) {
-  tatal.push(a);
-  document.getElementById("tatal").innerHTML = tatal.join("");
-  console.log(tatal);
-}
+console.log(total);
 
-tatal.forEach(function () {
-  for (i = 0; i < length; i++) {
-    return tatal;
+function Count(input) {
+  //rename
+  const len = total.length;
+  const prev = len > 0 ? total[len - 1] : undefined;
+  const isPrevNumber = !isNaN(prev);
+  const isInputNumber = !isNaN(input);
+  if (isPrevNumber && isInputNumber) {
+    total[len - 1] += String(input);
+  } else {
+    total.push(input);
   }
-});
-
-function answer() {
-  let ans = tatal.join("");
-  document.getElementById("tatal").innerHTML = ans;
+  console.log(typeof total);
+  document.getElementById("total").innerHTML = total.join("");
+  console.log(total);
 }
+
+function answer(total) {
+  // for (let i = 0; i < total.length; i++) {
+  //   if (i === "*") {
+  //     let a = parseInt(total(i - 1)) * parseInt(total(i + 1));
+  //     total.splice(i, 3, a);
+  //   } else if (i === "/") {
+  //     let b = parseInt(total(i - 1)) / parseInt(total(i + 1));
+  //     total.splice(i, 3, b);
+  //   }
+  //   console.log(total);
+  // }
+
+  // let ans = total.join("");
+  // document.getElementById("total").innerHTML = ans;
+  for (let i = 0; i < total.length; i++) {
+    if (total[i] === undefined) bretotalk;
+    if (total[i] === "*") {
+      total[i - 1] = total[i - 1] * total[i + 1];
+      total.splice(i, 2);
+      i--;
+    }
+    if (total[i] === "/") {
+      total[i - 1] = total[i - 1] / total[i + 1];
+      total.splice(i, 2);
+      i--;
+    }
+  }
+  for (let i = 0; i < total.length; i++) {
+    if (total[i] === undefined) bretotalk;
+    if (total[i] === "+") {
+      total[i - 1] = +total[i - 1] + +total[i + 1];
+      total.splice(i, 2);
+      i--;
+    }
+    if (total[i] === "-") {
+      total[i - 1] = +total[i - 1] - +total[i + 1];
+      total.splice(i, 2);
+      i--;
+    }
+  }
+  console.log(total);
+}
+
 function Clear() {
-  tatal = [];
-  document.getElementById("tatal").innerHTML = tatal;
+  total = [];
+  document.getElementById("total").innerHTML = total;
 }
